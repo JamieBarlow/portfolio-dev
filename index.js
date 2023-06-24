12dcb88f65594ae9b84336098536a00757f1aaf5
Jamie Barlow <jamiebarlow@gmail.com>
2023-06-24 12:53:26 +0100

    Add boilerplate templating, project data, and routing for project show pages

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate')
+const projectData = require('./projects/projectData');
 
 app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 // Serve static assets
 app.use(express.static(path.join(__dirname, 'assets')))
@@ -16,9 +19,14 @@ app.get('/projects', (req, res) => {
 })
 
 app.get('/projects/:project', (req, res) => {
-    const { project } = req.params;
-    res.render('projects', { project })
-    // res.send(`<h1>Viewing page for the ${project} project`);
+    let { project } = req.params;
+    if (project in projectData) {
+        const projectInfo = projectData[project];
+        res.render('projects/show', { projectInfo });
+      } else {
+        // Handle case when project is not found
+        res.status(404).send('Project not found');
+      }
 })
 
 app.get('*', (req, res) => {
---
+++ projects/projectData.js
@@ -0,0 +1,12 @@
+const projectData = {
+    slidingPuzzle: {
+        title: 'Sliding Puzzle',
+        description: 'This is the first project.'
+    },
+    directDebits: {
+        title: 'Direct Debit Processing Calendar',
+        description: 'This is the second project.'
+    }
+}
+
+module.exports = projectData;
