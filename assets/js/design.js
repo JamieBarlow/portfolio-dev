652a66bf5459434f3c5bd9c2be111da55d60dd40
Jamie Barlow <jamiebarlow@gmail.com>
2023-07-28 17:20:15 +0100

    Add burger menu and sidebar

--- assets/js/design.js
+++ assets/js/design.js
@@ -1,31 +1,44 @@
+// Slide in animations
 const slideElems = document.querySelectorAll(".slide-up, .slide-right, .slide-left");
 
 const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("active");
-            observer.unobserve(entry.target);
-        }
-    })
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+      observer.unobserve(entry.target);
+    }
+  })
 }
 
 const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.2
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2
 }
 
-const upObserver = new IntersectionObserver(callback, options)
-const rightObserver = new IntersectionObserver(callback, options);
-const leftObserver = new IntersectionObserver(callback, options);
-
+const observer = new IntersectionObserver(callback, options)
 
 slideElems.forEach(elem => {
-    if (elem.classList.contains("slide-up")) {
-        upObserver.observe(elem);
-    } else if (elem.classList.contains("slide-right")) {
-        rightObserver.observe(elem);
-    } else if (elem.classList.contains("slide-left")) {
-        leftObserver.observe(elem);
-    }
+  observer.observe(elem);
+});
+
+// Burger menu
+const button = document.querySelector("button");
+const sidebar = document.querySelector(".sidebar");
+const hamburger = document.querySelector('.hamburger');
+
+button.addEventListener("click", () => {
+  const currentState = button.getAttribute("data-state");
+
+  if (!currentState || currentState === "closed") {
+    button.setAttribute("data-state", "opened");
+    button.setAttribute("aria-expanded", "true");
+    sidebar.classList.add('active-sidebar');
+    hamburger.classList.add('active-burger');
+  } else {
+    button.setAttribute("data-state", "closed");
+    button.setAttribute("aria-expanded", "false");
+    sidebar.classList.remove('active-sidebar');
+    hamburger.classList.remove('active-burger');
+  }
 });
