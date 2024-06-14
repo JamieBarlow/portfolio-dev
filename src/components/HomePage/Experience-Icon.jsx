e2d1f2d0dd854c41b40f003ac4db13791e853f78
JamieBarlow <104205664+JamieBarlow@users.noreply.github.com>
2024-06-14 15:59:55 +0000

    Update slideObserver

--- src/components/HomePage/Experience-Icon.jsx
+++ src/components/HomePage/Experience-Icon.jsx
@@ -1,13 +1,11 @@
-import React, {useContext, forwardRef} from "react";
-import { ObserverContext } from "../../context/ObserverContext";
+import React, {forwardRef} from "react";
 
 const ExperienceIcon = forwardRef(({ tech, children }, ref) => {
-  const {slidingElemIsIntersecting} = useContext(ObserverContext);
   return (
     <div ref={ref}
       className={`experience__box slide-up ${
         tech === "HTML / CSS" ? "doubleicon" : ""
-      } ${slidingElemIsIntersecting && 'active'}`}
+      }`}
     >
       <h4>{tech}</h4>
       <div className="experience__icons">{children}</div>
--- src/components/HomePage/ProjectPreviewImg.jsx
+++ src/components/HomePage/ProjectPreviewImg.jsx
@@ -13,7 +13,7 @@ export default function ProjectPreviewImg({
       <picture>
         <source media="(min-width: 990px)" srcSet={picL} />
         <source media="(min-width: 200px)" srcSet={picM} />
-        <img className="slide-up" src={src} alt={title} />
+        <img className="slide-up active" src={src} alt={title} />
       </picture>
       <div className="img-overlay img-overlay--blur">
         <div className="img-overlay__button">
