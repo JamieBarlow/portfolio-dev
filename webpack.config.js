ef5248cde5360441ef69550e1be66bb70c6cfb77
JamieBarlow <104205664+JamieBarlow@users.noreply.github.com>
2024-06-21 15:37:11 +0000

    Update build config for netlify

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "index.js", // Replace with the path to your entry JavaScript file
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"), // Replace 'dist' with the desired output directory
-  },
-};
