41450b9a0a16f9790b1214f3b80f15a44b6e91fe
JamieBarlow <104205664+JamieBarlow@users.noreply.github.com>
2024-06-21 15:19:19 +0000

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: "index.js", // Replace with the path to your entry JavaScript file
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "assets"), // Replace 'dist' with the desired output directory
+    path: path.resolve(__dirname, "dist"), // Replace 'dist' with the desired output directory
   },
 };
